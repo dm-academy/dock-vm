12d4ec4f98c5433b676fd558c03ff0ab6f476cce
Charles Betz <betz4871@stthomas.edu>
2020-03-12 20:08:12 -0500

    main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "google" {
+  project = "proven-sum-252123"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "dock-vm" {
+  name         = "dock-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+resource "google_compute_firewall" "default" {
+ name    = "dock-vm-net"
+ network = "default"
+
+ allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "1000-2000"]
+  }
+}
+
+  output "ip" {
+     value = "${google_compute_instance.dock-vm.network_interface.0.access_config.0.nat_ip}"
+  }
